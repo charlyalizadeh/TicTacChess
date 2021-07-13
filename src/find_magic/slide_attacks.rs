bb5547032ecf81eeede4c457668a556c46598d16
charlyalizadeh <charlypro1502@outlook.fr>
2021-07-13 18:34:27 +0200

    Add: slide attacks generation file to find_magic module

--- src/find_magic/slide_attacks.rs
+++ src/find_magic/slide_attacks.rs
@@ -1,25 +1,25 @@
-use crate::constants::RAY_ATTACKS;
+use crate::constants;
 use crate::utils::bitutils;
-use crate::find_magic::pieces::IndexPiece;
+use super::pieces::IndexPiece;
 
 fn get_positive_attacks(occ: u32, direction: usize, square: usize) -> u32 {
-    let mut attacks: u32 = RAY_ATTACKS[direction][square];
+    let mut attacks: u32 = constants::RAY_ATTACKS[direction][square];
     let blockers: u32 = attacks & occ;
     if blockers != 0 {
         let mut square = bitutils::bit_scan_forward(blockers as u64) as usize;
         square -= square / 5;
-        attacks ^= RAY_ATTACKS[direction][square];
+        attacks ^= constants::RAY_ATTACKS[direction][square];
     }
     attacks
 }
 
 fn get_negative_attacks(occ: u32, direction: usize, square: usize) -> u32 {
-    let mut attacks: u32 = RAY_ATTACKS[direction][square];
+    let mut attacks: u32 = constants::RAY_ATTACKS[direction][square];
     let blockers: u32 = attacks & occ;
     if blockers != 0 {
         let mut square = bitutils::bit_scan_reverse(blockers as u64) as usize;
         square -= square / 5;
-        attacks ^= RAY_ATTACKS[direction][square];
+        attacks ^= constants::RAY_ATTACKS[direction][square];
     }
     attacks
 }
@@ -32,7 +32,7 @@ fn get_single_ray_attacks(occ: u32, direction: usize, square: usize) -> u32 {
     }
 }
 
-pub fn get_attacks(occ: u32, piece: &IndexPiece) -> u32 {
+pub fn get_slide_attacks(occ: u32, piece: &IndexPiece) -> u32 {
     match piece {
         IndexPiece::Rook(sq) => get_single_ray_attacks(occ, 0, *sq) |
                             get_single_ray_attacks(occ, 2, *sq) |
